96e1da60ad8268628a5b824becef94c1e104ddc5
qiliu <date20150619@icloud.com>
2024-04-03 17:50:49 +0800

    添加通用工具

---
+++ lead-news-common/src/main/java/com/surge/constant/AdminConstant.java
@@ -0,0 +1,8 @@
+package com.surge.constant;
+
+public class AdminConstant {
+    public static final Short WAIT_AUTH = 1;
+    public static final Short PASS_AUTH = 9;
+    public static final Short FAIL_AUTH = 2;
+    public static final Short AUTHOR_TYPE = 2;
+}
---
+++ lead-news-common/src/main/java/com/surge/dto/ResponseResult.java
@@ -0,0 +1,8 @@
+package com.surge.dto;
+
+import java.io.Serializable;
+
+public class ResponseResult<T> implements Serializable {
+
+
+}
