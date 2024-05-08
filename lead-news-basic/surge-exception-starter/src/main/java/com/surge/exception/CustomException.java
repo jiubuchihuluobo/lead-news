e381c24ef435ec002cea75167e4451fdf343a38f
qiliu <date20150619@ilcoud.com>
2024-05-08 14:02:40 +0800

    修改异常类

--- lead-news-basic/surge-exception-starter/src/main/java/com/surge/exception/CustomException.java
+++ lead-news-basic/surge-exception-starter/src/main/java/com/surge/exception/CustomException.java
@@ -1,30 +1,35 @@
 package com.surge.exception;
 
 import com.surge.common.enums.HttpCodeEnum;
-import lombok.Getter;
 
-@Getter
+
 public class CustomException extends RuntimeException {
 
     private final HttpCodeEnum httpCodeEnum;
 
     private final String message;
 
+    /**
+     * 构造一个新的CustomException实例，使用给定的http状态码枚举来设置异常消息。
+     *
+     * @param httpCodeEnum HTTP状态码枚举
+     */
     public CustomException(HttpCodeEnum httpCodeEnum) {
         super(httpCodeEnum.getErrorMessage());
         this.httpCodeEnum = httpCodeEnum;
         this.message = httpCodeEnum.getErrorMessage();
     }
 
+    /**
+     * 构造一个新的CustomException实例，使用给定的http状态码枚举和消息来设置异常消息。
+     *
+     * @param httpCodeEnum HTTP状态码枚举
+     * @param message      异常消息
+     */
     public CustomException(HttpCodeEnum httpCodeEnum, String message) {
         super(message);
         this.httpCodeEnum = httpCodeEnum;
         this.message = message;
-    }
-
-    @Override
-    public String getMessage() {
-        return this.message;
     }
 
 }
