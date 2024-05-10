5145ccb1f79a098989429adab4d10208ccc22f27
qiliu <date20150619@ilcoud.com>
2024-05-10 18:53:12 +0800

    移除接口上的@Service

--- lead-news-service/admin-service/src/main/java/com/surge/admin/service/AdminUserService.java
+++ lead-news-service/admin-service/src/main/java/com/surge/admin/service/AdminUserService.java
@@ -4,11 +4,8 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.surge.admin.dto.AdminUserDTO;
 import com.surge.admin.pojo.AdminUser;
 import com.surge.common.dto.ResponseResult;
-import org.springframework.stereotype.Service;
 
-import java.util.Map;
 
-@Service
 public interface AdminUserService extends IService<AdminUser> {
 
     ResponseResult<Object> login(AdminUserDTO dto);
